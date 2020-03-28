39addd67f4d34c18e71c3e69fa1d3b57e0c32521
Abhijay Saini <abhijay.saini@gmail.com>
2020-03-28 12:05:22 -0500

    first commit

---
+++ DifferentNumbers.java
@@ -0,0 +1,14 @@
+/*
+ * Source: https://youtu.be/jqJ5s077OKo?list=PLl0KD3g-oDOHpWRyyGBUJ9jmul0lUOD80&t=495
+ * Problem: You are given N <= 10^7 numbers, each between 0 and 10^9. How many different values appear in the sequence
+ */
+
+class DifferentNumbers {
+    public static void main(String[] args) {
+        // the fastest method is to create a bitset of size 10^9
+        // to keep track of all the numbers
+        // the smallest possible number is 0
+        // the largest possible number is 10^9
+        // we can check off whether or not we have seen a number in the bitset
+    }
+}
