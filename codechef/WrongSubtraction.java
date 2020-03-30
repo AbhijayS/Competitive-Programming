45330f3f9495b3fb070bca69104aeed28a2fe282
Abhijay Saini <abhijay.saini@gmail.com>
2020-03-29 19:22:12 -0500

    wrong subtraction problem completed

---
+++ codechef/WrongSubtraction.java
@@ -0,0 +1,22 @@
+package codechef;
+
+import java.util.Scanner;
+
+class WrongSubtraction {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int N = scanner.nextInt();
+        int K = scanner.nextInt();
+        for (int i = 0; i < K; i++) {
+            N = subtractOne(N);
+        }
+        System.out.println(N);
+        scanner.close();
+    }
+
+    static int subtractOne(int original) {
+        if (original % 10 == 0)
+            return original /= 10;
+        return original - 1;
+    }
+}
